944fbe6ca270726faee57c8fc6d255224be6cfc2
fribeiro1 <webmaster@fernandoribeiro.eti.br>
2012-09-22 15:20:00 -0300

    Initial commit

---
+++ src/main/java/br/eti/fernandoribeiro/sample/spring/SampleController.java
@@ -0,0 +1,21 @@
+package br.eti.fernandoribeiro.sample.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public final class SampleController {
+
+	@RequestMapping("/sendMessage")
+	public ModelAndView sayHello(@RequestParam("message") final String message) {
+		final ModelAndView modelAndView = new ModelAndView(
+				"/WEB-INF/jsp/message.jsp");
+
+		modelAndView.addObject("message", message);
+
+		return modelAndView;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/message.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Spring Sample Application</title>
+</head>
+<body>
+	<p>${message}</p>
+	<p>
+		<a href="index.jsp">Back</a>
+	</p>
+</body>
+</html>
